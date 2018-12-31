c11a752fe07aba2a5e8ab522ed6e169b2a27019a
Nifnifas <31838340+Nifnifas@users.noreply.github.com>
2018-12-31 11:45:34 +0200

    Start of a project
    -index.php
    -includes folder
    -login.php form
    -stylesheets

---
+++ include/nustatymai.php
@@ -0,0 +1,30 @@
+﻿<?php
+//nustatymai.php
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "korepetitoriai");
+define("TBL_USERS", "users");
+define("TBL_ARTICLES", "articles");
+define("TBL_CATEGORIES", "categories");
+define("TBL_COMMENTS", "comments");
+define("TBL_STATUS", "status");
+
+$user_roles=array(      // vartotojų rolių vardai lentelėse ir  atitinkamos userlevel reikšmės
+	"Redaktorius"=>"9",
+	"Skaitytojas"=>"5",
+	"Skaitytojas*"=>"4");   // galioja ir vartotojas "guest", kuris neturi userlevel
+define("DEFAULT_LEVEL","Skaitytojas");  // kokia rolė priskiriama kai registruojasi
+define("ADMIN_LEVEL","Redaktorius");  // kas turi vartotojų valdymo teisę
+define("UZBLOKUOTAS","255");      // vartotojas negali prisijungti kol administratorius nepakeis rolės
+
+$uregister="both";  // kaip registruojami vartotojai
+// self - pats registruojasi, admin - tik ADMIN_LEVEL, both - abu atvejai
+
+// * Email Constants - 
+define("EMAIL_FROM_NAME", "Demo");
+define("EMAIL_FROM_ADDR", "demo@ktu.lt");
+define("EMAIL_WELCOME", false);
+
+?>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <link href="include/styles.css" rel="stylesheet" type="text/css" >
+        <title></title>
+    </head>
+    <body>
+        <?php
+            session_start();
+            include("include/functions.php");
+
+            if (!empty($_SESSION['user']))     //Jei vartotojas prisijungęs, valom logino kintamuosius ir rodom meniu
+            {                                  // Sesijoje nustatyti kintamieji su reiksmemis is DB
+                                               // $_SESSION['user'],$_SESSION['ulevel'],$_SESSION['userid'],$_SESSION['umail']
+
+                        inisession("part");   //   pavalom prisijungimo etapo kintamuosius
+                        $_SESSION['prev']="index"; 
+
+
+                include("include/meniu.php"); //įterpiamas meniu pagal vartotojo rolę
+        ?>
+
+                        <div style="text-align: center;color:green">
+                            <br><br>
+                            <h1>Pradinis sistemos puslapis.</h1>
+                            <h1>Projektą parengė Lukas Krupenkinas, IFF-6/5</h1>
+                        </div><br>
+        <?php
+            }                
+                  else {   			 
+
+                      if (!isset($_SESSION['prev'])) inisession("full");             // nustatom sesijos kintamuju pradines reiksmes 
+                      else {if ($_SESSION['prev'] != "proclogin") inisession("part"); // nustatom pradines reiksmes formoms
+                           }  
+                                  // jei ankstesnis puslapis perdavė $_SESSION['message']
+                                        echo "<div align=\"center\">";echo "<font size=\"4\" color=\"#ff0000\">".$_SESSION['message'] . "<br></font>";          
+
+                        echo "<table class=\"center\"><tr><td>";
+                  include("include/login.php");                    // prisijungimo forma
+                        echo "</td></tr></table></div><br><br>";
+
+                          }
+        ?>
+    </body>
+</html>
